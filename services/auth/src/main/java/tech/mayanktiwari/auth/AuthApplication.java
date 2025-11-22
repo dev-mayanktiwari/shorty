2e96d6777662c345a106a05f7eadbb567b5c8f29
Mayank Tiwari <mayanktiwari1212.mt@gmail.com>
2025-11-23 01:37:01 +0530

    feat: app startup

--- services/auth/src/main/java/tech/mayanktiwari/auth/AuthApplication.java
+++ services/auth/src/main/java/tech/mayanktiwari/auth/AuthApplication.java
@@ -1,7 +1,13 @@
 package tech.mayanktiwari.auth;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "tech.mayanktiwari.auth", "tech.mayanktiwari.common" })
 public class AuthApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
