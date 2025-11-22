07c2b0113417b71fbcb6f83459b404cfb12a9b06
Mayank Tiwari <mayanktiwari1212.mt@gmail.com>
2025-11-22 16:49:22 +0530

    feat: updated auth controller

---
+++ services/auth/src/main/java/tech/mayanktiwari/auth/controllers/AuthController.java
@@ -0,0 +1,37 @@
+package tech.mayanktiwari.auth.controllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import tech.mayanktiwari.auth.dto.response.RegisterRequest;
+import tech.mayanktiwari.auth.dto.response.UserResponseDto;
+import tech.mayanktiwari.auth.service.AuthService;
+import tech.mayanktiwari.common.api.ApiResponse;
+import tech.mayanktiwari.common.builder.ResponseBuilder;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<UserResponseDto>> registerUser(
+            @RequestBody
+            @Valid
+            RegisterRequest registerRequest
+    ) {
+        UserResponseDto response = authService.registerUser(registerRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(ResponseBuilder.success(response, "User registered successfully",
+                                                           HttpStatus.CREATED));
+
+    }
+}
